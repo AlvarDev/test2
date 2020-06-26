faac1e5c8a611856b89229ff5e5f622f9b0b9937
AlvarDev <alvardev.lp@gmail.com>
2020-06-26 16:50:20 -0300

    My API

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cloud-build-demo-2
 This is a demo
+
+
+curl -X POST \
+    http://localhost:8080/echo \
+    -H 'Content-Type: application/json' \
+    -d '{"name": "AlvarDev"}'
---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/echo', methods=['POST'])
+def echo():
+	body = request.get_json()
+	return jsonify(body), 200
